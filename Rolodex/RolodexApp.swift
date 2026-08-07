4b3aaf5ccd325e4faab3e6a25dbfd9649bd5b8c7
Carlos Yáñez <carlosyanez997@gmail.com>
2026-08-07 03:43:44 +0200

    Initial Commit

---
+++ Rolodex/RolodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RolodexApp.swift
+//  Rolodex
+//
+//  Created by Carlos Yanez Puig on 07/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct RolodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
