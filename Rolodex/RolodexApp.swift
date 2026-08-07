7a680414215daec91082ee03109657af473dc58f
Carlos Yáñez <carlosyanez997@gmail.com>
2026-08-07 20:35:55 +0200

    Add modelContainer to RolodexApp body

--- Rolodex/RolodexApp.swift
+++ Rolodex/RolodexApp.swift
@@ -5,6 +5,7 @@
 //  Created by Carlos Yanez Puig on 07/08/2026.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct RolodexApp: App {
         WindowGroup {
             UserListView()
         }
+        .modelContainer(for: User.self)
     }
 }
