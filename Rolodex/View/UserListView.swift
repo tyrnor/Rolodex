d0d7646a1fb6dc10dd9c7687de8170ae8a5863a7
Carlos Yáñez <carlosyanez997@gmail.com>
2026-08-07 04:37:46 +0200

    Create UserListView and update RolodexApp window group to use it

---
+++ Rolodex/View/UserListView.swift
@@ -0,0 +1,45 @@
+//
+//  UserListView.swift
+//  Rolodex
+//
+//  Created by Carlos Yanez Puig on 07/08/2026.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    @State private var viewModel = UserListViewModel()
+    
+    
+    var body: some View {
+        NavigationStack {
+            List(viewModel.users) { user in
+                NavigationLink(value: user) {
+                    HStack {
+                        Text(user.name)
+                        Spacer()
+                        Circle()
+                            .fill(user.isActive ? .green : .red)
+                            .frame(width: 10, height: 10)
+                    }
+                }
+            }
+            .navigationTitle("Rolodex")
+            .navigationDestination(for: User.self, destination: { user in
+                
+            })
+            .task {
+                await viewModel.loadUsers()
+            }
+            .alert("Error", isPresented: $viewModel.showingError) {
+                
+            } message: {
+                Text(viewModel.errorMessage)
+            }
+        }
+    }
+}
+
+#Preview {
+    UserListView()
+}
